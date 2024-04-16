0daf7b05dbee905353416b6ea90601fb2ed586e0
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-04-17 00:17:01 +0200

    specify features / parser

--- extract.py
+++ extract.py
@@ -28,7 +28,7 @@ if result.returncode != 0:
 index = root / "0"
 
 with index.open() as f:
-    soup = bs4.BeautifulSoup(f.read())
+    soup = bs4.BeautifulSoup(f.read(), "lxml-xml")
 
 for payload in soup.find_all("payload"):
     maybe_filepath: str = payload.get("filepath")
