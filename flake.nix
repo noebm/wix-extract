ada7bad4261334151f25c52fd7bfec78ab9dc73d
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-12-11 13:30:39 +0100

    refactor: update flake
    1. use the python specific build-system and dependencies fields
    2. formatting

--- flake.nix
+++ flake.nix
@@ -9,16 +9,15 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.default =
-        pkgs.python3Packages.buildPythonPackage {
-          pname = "wix-extract";
-          version = "0.0.1";
-          pyproject = true;
-          src = ./.;
+      packages.default = pkgs.python3Packages.buildPythonPackage {
+        pname = "wix-extract";
+        version = "0.0.1";
+        pyproject = true;
+        src = ./.;
 
-          buildInputs = [pkgs.python3Packages.setuptools];
-          propagatedBuildInputs = [pkgs.cabextract] ++ (with pkgs.python3Packages; [beautifulsoup4 lxml]);
-        };
+        build-system = [pkgs.python3Packages.setuptools];
+        dependencies = [pkgs.cabextract] ++ (with pkgs.python3Packages; [beautifulsoup4 lxml]);
+      };
       apps.default = flake-utils.lib.mkApp {drv = self.packages.${system}.default;};
     });
 }
