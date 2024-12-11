2f25a6851b3116801cbf2c780608c5cd802c3e12
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-12-11 13:57:14 +0100

    use pyproject-nix to load pyproject.toml
    this fixes double dependency definitions in flake.nix and pyproject toml

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   inputs.nixpkgs.url = "nixpkgs";
+  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = {
     self,
     nixpkgs,
+    pyproject-nix,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.default = pkgs.python3Packages.buildPythonPackage {
-        pname = "wix-extract";
-        version = "0.0.1";
-        pyproject = true;
-        src = ./.;
-
-        build-system = [pkgs.python3Packages.setuptools];
-        dependencies = [pkgs.cabextract] ++ (with pkgs.python3Packages; [beautifulsoup4 lxml]);
-      };
+      packages.default = let
+        project = pyproject-nix.lib.project.loadPyproject {
+          # Read & unmarshal pyproject.toml relative to this project root.
+          # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
+          projectRoot = ./.;
+        };
+        packageAttrs = project.renderers.buildPythonPackage {python = pkgs.python3;};
+      in
+        pkgs.python3Packages.buildPythonPackage (
+          packageAttrs // {dependencies = packageAttrs.dependencies ++ [pkgs.cabextract];}
+        );
       apps.default = flake-utils.lib.mkApp {drv = self.packages.${system}.default;};
     });
 }
